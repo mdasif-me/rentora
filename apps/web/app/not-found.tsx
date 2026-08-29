6c2be1549138e67031f70379038a0f899b766a36
mdasif-me <cn.mdasif@gmail.com>
2026-08-29 19:41:29 +0600

    feat: add a new file for managed 404 not found

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { NotFoundGlitch } from "@/components/motion/not-found/glitch";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center flex-col h-screen">
+      <NotFoundGlitch />
+    </div>
+  );
+}
